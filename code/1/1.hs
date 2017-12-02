f1dd01fb6f01fc8d993cf2d6bf506797e2fb6b5d
Jonas Kastberg Hinrichsen <jkas@itu.dk>
2017-12-02 16:54:59 +0100

    advent 1 done

---
+++ code/1/1.hs
@@ -0,0 +1,9 @@
+import Data.Char
+
+main = do 
+  input <- getContents
+  let offset = div (length l1) 2
+  let l1 = map digitToInt (filter isDigit input)
+  let l2 = (drop offset l1) ++ (take offset l1)
+  let l = zip l1 l2
+  print (foldl (\a (x,y) -> if x==y then a+x else a) 0 l)
