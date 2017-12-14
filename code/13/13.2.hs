ac60ef48566825ccb52da597920477348680f5b7
Jonas Kastberg Hinrichsen <jkas@itu.dk>
2017-12-14 18:32:28 +0100

    Solved 13

--- code/13/13.2.hs
+++ code/13/13.2.hs
@@ -1,37 +1,26 @@
 import Data.List.Split;
 
-
+mkScanners :: [String] -> Integer -> [Maybe (Integer,Integer)]
 mkScanners [] i = []
 mkScanners (s:ss) i = 
   let s' = splitOn ": " s in
   let (id,len) = (read (head s'), read (head (tail s'))) in
   if i == id 
-    then Just (0,len,id,True) : mkScanners ss (i+1)
+    then Just (id,len) : mkScanners ss (i+1)
     else Nothing : mkScanners (s:ss) (i+1)
 
-updateScanner Nothing = Nothing
---updateScanner (Just (p,l,d,o)) = Just (p+o,l,d, if (p+o) == (l-1) then -o else o)
-updateScanner (Just (p,l,d,True)) = Just (p+1,l,d, if (p+1) == (l-1) then False else True)
-updateScanner (Just (p,l,d,False)) = Just (p-1,l,d, if (p-1) == 0 then True else False)
+checkHit Nothing i = 0
+checkHit (Just (d,l)) i = if (mod i ((l*2)-2)) == 0 then 1 else 0
 
-updateScanners [] = []
-updateScanners (s:ss) = updateScanner s : updateScanners ss
+walk [] i = 0
+walk (s:ss) i = checkHit s i + walk ss (i+1)
 
-checkHit Nothing = 0
-checkHit (Just (p,l,d,o)) = if p == 0 then l*d else 0
-
-walk [] = 0
-walk (s:ss) = checkHit s + walk (updateScanners ss) 
-
-delay ss 0 = ss
-delay ss n = delay (updateScanners ss) (n-1)
 
 lowestNeededDelay ss d =
-  let ss' = delay ss d in
-  let isSuccess = walk ss' == 0 in
+  let isSuccess = walk ss d == 0 in
   if isSuccess then d else lowestNeededDelay ss (d+1)
 
-lowestNeededDelay2 ss = 
+
 
 main = do
   input <- getContents
